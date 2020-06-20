fbb0eb4883100af2b06521afb8b290cf01a296be
brandon <brandon.fitzpatrick8154@gmail.com>
2020-06-20 14:03:21 -0500

    spot class done

--- src/sample/Move.java
+++ src/sample/Move.java
@@ -6,5 +6,42 @@ public class Move {
     private Piece movePiece;
     private Piece killPiece;
 
+    public Move(Spot startSpot, Spot finishSpot, Piece movePiece, Piece killPiece) {
+        this.startSpot = startSpot;
+        this.finishSpot = finishSpot;
+        this.movePiece = movePiece;
+        this.killPiece = killPiece;
+    }
 
+    public Spot getStartSpot() {
+        return startSpot;
+    }
+
+    public void setStartSpot(Spot startSpot) {
+        this.startSpot = startSpot;
+    }
+
+    public Spot getFinishSpot() {
+        return finishSpot;
+    }
+
+    public void setFinishSpot(Spot finishSpot) {
+        this.finishSpot = finishSpot;
+    }
+
+    public Piece getMovePiece() {
+        return movePiece;
+    }
+
+    public void setMovePiece(Piece movePiece) {
+        this.movePiece = movePiece;
+    }
+
+    public Piece getKillPiece() {
+        return killPiece;
+    }
+
+    public void setKillPiece(Piece killPiece) {
+        this.killPiece = killPiece;
+    }
 }
--- src/sample/Spot.java
+++ src/sample/Spot.java
@@ -15,11 +15,23 @@ public class Spot {
         return x;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
     public int getY() {
         return y;
     }
 
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public Piece getPiece() {
         return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
     }
 }
