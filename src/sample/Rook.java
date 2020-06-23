9678f7bea72bfb0aee1f3950827ed2d761c0880e
brandon <brandon.fitzpatrick8154@gmail.com>
2020-06-23 16:27:53 -0500

    building out rook class

--- src/sample/Rook.java
+++ src/sample/Rook.java
@@ -2,4 +2,12 @@ package sample;
 
 public class Rook extends Piece{
 
+    public Rook(boolean isWhite) {
+        super(isWhite);
+    }
+
+    @Override
+    public boolean canMove(Board board, Spot startSpot, Spot endSpot) {
+        return false;
+    }
 }
