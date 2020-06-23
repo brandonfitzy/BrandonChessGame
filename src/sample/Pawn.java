4af41c8abf920e1b612524a9d80c3f05dca0912f
brandon <brandon.fitzpatrick8154@gmail.com>
2020-06-23 16:52:23 -0500

    bulding out pawn class

--- src/sample/Pawn.java
+++ src/sample/Pawn.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class Pawn {
+public class Pawn extends Piece {
+    private boolean firstMove;
+
+
+    public Pawn(boolean isWhite) {
+        super(isWhite);
+        this.firstMove = true;
+    }
+
+    @Override
+    public boolean canMove(Board board, Spot startSpot, Spot endSpot) {
+        if (firstMove){
+
+        } else {
+
+        }
+    }
+
 }
